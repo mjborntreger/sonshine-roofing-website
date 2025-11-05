465eaaf6cf3c33693f381f2539bd8c9dfe950240
Michael Borntreger <54005708+mjborntreger@users.noreply.github.com>
2025-11-05 17:14:47 -0500

    added modernize footer badge

--- components/global-nav/footer/FooterBadges.tsx
+++ components/global-nav/footer/FooterBadges.tsx
@@ -56,11 +56,16 @@ const badges = [
     title: "Official Tri-County Metals Installer",
     src: "https://next.sonshineroofing.com/wp-content/uploads/tri-county-metals-logo-transparent-bg.webp",
   },
+  {
+    href: "https://modernize.com",
+    title: "Modernize Home Services",
+    src: "https://next.sonshineroofing.com/wp-content/uploads/modernizelogo-bg-transparent-ezgif.com-optiwebp.webp",
+  },
 ];
 
 export default function FooterBadges() {
   return (
-    <div className="mx-auto max-w-6xl px-10 grid grid-cols-3 lg:grid-cols-10 place-items-center justify-center gap-3 my-20">
+    <div className="mx-auto max-w-6xl px-10 grid grid-cols-3 lg:grid-cols-11 place-items-center justify-center gap-3 my-20">
       {badges.map((badge) => (
         <SmartLink
           key={badge.href}
