bc6696a92477a5a91e2688a1d3546d6ca8d9d442
Michael Borntreger <54005708+mjborntreger@users.noreply.github.com>
2026-09-24 16:27:15 -0400

    Remove unused partnership accordion import

--- components/location/LocalPartnershipsSection.tsx
+++ components/location/LocalPartnershipsSection.tsx
@@ -3,7 +3,6 @@ import { Facebook, Globe, Instagram } from "lucide-react";
 import type { SponsorFeature } from "@/lib/content/sponsor-features";
 import { SECTION_HEADING, SECTION_SUBTITLE } from "@/components/location/sectionStyles";
 import { renderHighlight } from "@/components/utils/renderHighlight";
-import { Accordion } from "@/components/ui/Accordion";
 import SmartLink from "../utils/SmartLink";
 
 type LocalPartnershipsSectionProps = {
